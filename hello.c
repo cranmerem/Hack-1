4abc75d2dd60cad2409485c9dcfad5484f5ded13
Emma Cranmer <emmacranmr00@gmail.com>
2018-08-23 12:59:27 -0500

    HAck 1

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Emma Cranmer  
+ * Date: 2018/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Emma Cranmer Computer Science Engineering\n");
+
+  return 0;
+}
