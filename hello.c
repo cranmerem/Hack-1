aaa1bf0a275ad6413b1e40f9805729d8bb52b6ff
Derek Moore <moorder1112@gmail.com>
2018-08-23 13:12:00 -0500

    Adding My Name

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Emma Cranmer  
+ * Author: Emma Cranmer & Derek Moore
  * Date: 2018/8/23
  *
  * A simple hello world program in C
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Emma Cranmer Computer Science Engineering\n");
+  printf("Emma Cranmer Computer Science Engineering, Derek Moore-Electrical Engineering\n");
 
   return 0;
 }
